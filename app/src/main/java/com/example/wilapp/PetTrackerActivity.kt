90e627bd1b4a986d798db5e17df3840fff993e2e
Biggie4life <lottringt12018@gmail.com>
2023-11-07 13:53:25 +0000

    Added current location

--- app/src/main/java/com/example/wilapp/PetTrackerActivity.kt
+++ app/src/main/java/com/example/wilapp/PetTrackerActivity.kt
@@ -6,10 +6,33 @@ import android.os.Bundle
 import com.example.wilapp.databinding.ActivityPetTrackerBinding
 import com.mapbox.navigation.ui.maps.route.RouteLayerConstants
 import com.mapbox.navigation.ui.maps.route.arrow.model.RouteArrowOptions
+import com.mapbox.navigation.ui.maps.route.line.model.MapboxRouteLineOptions
+import com.mapbox.navigation.ui.maps.route.line.model.RouteLineColorResources
+import com.mapbox.navigation.ui.maps.route.line.model.RouteLineResources
 
 class PetTrackerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPetTrackerBinding
+    private var toggleCustomOptions = false
+
+    private val routeLineOptions: MapboxRouteLineOptions by lazy {
+        MapboxRouteLineOptions.Builder(this)
+            .withRouteLineResources(
+                RouteLineResources.Builder()
+                    .routeLineColorResources(
+                        RouteLineColorResources.Builder()
+                            .routeLowCongestionColor(Color.YELLOW)
+                            .routeCasingColor(Color.RED)
+                            .build()
+                    )
+                    .build()
+            )
+// make sure to use the correct layerId based on the map styles
+            .withRouteLineBelowLayerId("road-label") // for Style.LIGHT and Style.DARK
+            .withVanishingRouteLineEnabled(true)
+            .displaySoftGradientForTraffic(true)
+            .build()
+    }
 
     private val routeArrowOptions by lazy {
         RouteArrowOptions.Builder(this)
